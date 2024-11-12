cd925fccbc7ab036c9ab8e2db5f4915e811192e2
807502278 <807502278@qq.com>
2024-11-12 15:57:44 +0800

    Optimize the loading of phonemizer
    Theoretically, loading phonemizer now should not conflict with packages in the environment.

--- __init__.py
+++ __init__.py
@@ -4,18 +4,28 @@ import importlib.util
 import os
 import sys
 
-load_path = os.path.dirname(__file__)
-if load_path not in sys.path: sys.path.insert(0, load_path)
+self_path = os.path.dirname(os.path.realpath(__file__))
+#if load_path not in sys.path: sys.path.insert(0, self_path)
 
-extension_folder = os.path.dirname(os.path.realpath(__file__))
-NODE_CLASS_MAPPINGS = {}
-NODE_DISPLAY_NAME_MAPPINGS = {}
-pyPath = os.path.join(extension_folder,'nodes')
+class TempSysPath:
+    def __init__(self, path_to_add):
+        self.path_to_add = path_to_add
+        self.original_path = sys.path.copy()
 
-def loadCustomNodes():
+    def __enter__(self):
+        sys.path.insert(0, self.path_to_add)
+
+    def __exit__(self, type, value, traceback):
+        sys.path = self.original_path
+
+with TempSysPath(self_path):
+    NODE_CLASS_MAPPINGS = {}
+    NODE_DISPLAY_NAME_MAPPINGS = {}
+    pyPath = os.path.join(self_path,'nodes')
     find_files = glob.glob(os.path.join(pyPath, "*.py"), recursive=True)
+    
     for file in find_files:
-        file_relative_path = file[len(extension_folder):]
+        file_relative_path = file[len(self_path):]
         model_name = file_relative_path.replace(os.sep, '.')
         model_name = os.path.splitext(model_name)[0]
         module = importlib.import_module(model_name, __name__)
@@ -27,8 +37,5 @@ def loadCustomNodes():
                 NODE_DISPLAY_NAME_MAPPINGS.update(module.NODE_DISPLAY_NAME_MAPPINGS)
         if hasattr(module, "init"):# init
             getattr(module, "init")()
-
-loadCustomNodes()# load Custom Nodes
-
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+    __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
 
